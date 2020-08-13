486a7eec9f393577982fb2738ece0c49276adfb7
mi78108 <mi78108@live.com>
2020-08-13 17:51:45 +0800

    添加 粘贴板功能

--- www/js/baseFunc.js
+++ www/js/baseFunc.js
@@ -125,6 +125,17 @@ function webRtc() {
             console.log("创建offer失败");
         })
     }
-
-
 }
+
+function copyToclip(txt) {
+             if (document.execCommand("copy")) {
+                   const input = document.createElement("input"); // 创建一个新input标签
+                   input.setAttribute("readonly", "readonly"); // 设置input标签只读属性
+                   input.setAttribute("value", txt); // 设置input value值为需要复制的内容
+                   document.body.appendChild(input); // 添加input标签到页面
+                   input.select(); // 选中input内容
+                   input.setSelectionRange(0, 9999); // 设置选中input内容范围
+                   document.execCommand("copy"); // 复制
+                   document.body.removeChild(input);  // 删除新创建的input标签
+                 }
+           }
